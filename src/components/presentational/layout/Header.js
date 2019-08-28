ecbf5504cf9ba54906272346a140a1a6a6a97104
Khoa Phan <khoaphan89@gmail.com>
2019-08-28 09:45:52 -0400

    Remove redundant Grid from Header

--- src/components/presentational/layout/Header.js
+++ src/components/presentational/layout/Header.js
@@ -1,6 +1,5 @@
 import React, { memo } from 'react';
 import {
-  Grid,
   AppBar,
   Toolbar,
 } from '@material-ui/core';
@@ -12,11 +11,9 @@ const styles = {
 };
 
 const Header = memo(({ children }) => (
-  <Grid container component="header" alignContent="center">
-    <AppBar position="static" color="primary" elevation={0}>
-      <Toolbar disableGutters variant="dense" style={styles.toolbar}>{ children }</Toolbar>
-    </AppBar>
-  </Grid>
+  <AppBar position="static" color="primary" elevation={0}>
+    <Toolbar disableGutters variant="dense" style={styles.toolbar}>{ children }</Toolbar>
+  </AppBar>
 ));
 
 export default Header;
