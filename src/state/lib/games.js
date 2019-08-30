da722c75019bf6d99359bdb3dc243025725aaf3b
Khoa Phan <khoaphan89@gmail.com>
2019-08-30 12:54:00 -0400

    Use moment-timezone to format dateTime to local user time

--- src/state/lib/games.js
+++ src/state/lib/games.js
@@ -1,6 +1,12 @@
 import { normalize, schema } from 'normalizr';
+import moment from 'moment-timezone';
 
-const gameSchema = new schema.Entity('games');
+const gameSchema = new schema.Entity('games', {}, {
+  processStrategy: value => ({
+    ...value,
+    dateTime: moment(value.dateTime).tz(Intl.DateTimeFormat().resolvedOptions().timeZone).format('MMMM D [at] h:mm A z'),
+  })
+});
 const gameListSchema = new schema.Array(gameSchema);
 
 const normalizeGameList = data => normalize(data, gameListSchema);
