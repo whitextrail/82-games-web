9ba238181a79876d380a19e32beffaa33acae538
Khoa Phan <khoaphan89@gmail.com>
2019-09-02 10:37:47 -0400

    Remove redundant status-setting logic in actionWrapper

--- src/state/lib/actions.js
+++ src/state/lib/actions.js
@@ -1,24 +1,9 @@
-// Outputs an action creator that can handle generic, success, and error cases
+// Cleanly wraps actions
 const actionWrapper = ({ type }) => {
-  const action = { type };
-  const hasOwnProp = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop);
-
-  return (payload = {}) => {
-    action.response = null;
-    action.error = null;
-    action.status = '';
-
-    if (hasOwnProp(payload, 'error')) {
-      action.error = payload.error;
-      action.status = 'error';
-    } else if (hasOwnProp(payload, 'response')) {
-      action.response = payload.response;
-      action.status = 'success';
-    }
-
-    // Return shallow copy of action
-    return { ...action };
-  };
+  return (payload = {}) => ({
+    ...payload,
+    type,
+  });
 };
 
 export { actionWrapper };
